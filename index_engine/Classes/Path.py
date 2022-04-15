7049ed69dd34a052ed922aa434470cef88d5b49e
krbuettner <78238895+krbuettner@users.noreply.github.com>
2022-04-14 22:31:59 -0400

    path codes

---
+++ index_engine/Classes/Path.py
@@ -0,0 +1,17 @@
+
+# uncompleted address of preprocessed corpus.
+#ResultHM1="data//result."
+
+# address of generated Text index file.
+IndexTextDir="data//indextext//"
+
+# address of generated Web index file.
+IndexWebDir="data//indexweb//"
+
+ResultHM1="data//all_docs.txt"
+
+# Added topic directory
+TopicDir = "data//topics.txt"
+
+# Added stopword directory
+StopwordDir = "data//stopword.txt"
