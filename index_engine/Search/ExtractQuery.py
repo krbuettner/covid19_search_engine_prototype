b6d05566f894424b852660ccff2a119c5ec76a63
krbuettner <78238895+krbuettner@users.noreply.github.com>
2022-04-14 22:32:35 -0400

    Add files via upload

---
+++ index_engine/Search/ExtractQuery.py
@@ -0,0 +1,70 @@
+import Classes.Query as Query
+import Classes.Path as Path
+from nltk.stem.porter import *
+
+class ExtractQuery:
+
+	def __init__(self):
+
+		# 1. you should extract the 4 queries from the Path.TopicDir
+		# 2. the query content of each topic should be 1) tokenized, 2) to lowercase, 3) remove stop words, 4) stemming
+		# 3. you can simply pick up title only for query.
+
+		# Load queries by parsing through tags
+		self.queries = []
+		with open(Path.TopicDir, "r") as f:
+			all_lines = f.readlines()
+		topics = []
+		count = -1
+		for line in all_lines:
+			if '<top>' in line:
+				count = count + 1
+				topics.append([])
+			topics[count].append(line.strip())
+		queries = []
+		for topic in topics:
+			for element in topic:
+				if '<title>' in element:
+					element = element.replace('<title>','').strip()
+					queries.append(element.lower())						# Make lowercase here
+
+		# Tokenize queries into words
+		queries_split = [[] for i in range(len(queries))]
+		index = 0
+		for q in queries:
+			queries_split[index] = q.split()
+			index = index + 1
+
+		# Load and remove stopwords, remove nonalphanumeric char, perform stemming
+		stemmer = PorterStemmer()
+		with open(Path.StopwordDir, 'r') as f:
+			self.stop_words = []
+			for l in f.readlines():
+				self.stop_words.append(l.strip())
+		queries_split_wo_stop = [[] for i in range(len(queries_split))]
+		for i in range(len(queries_split)):
+			queries_split_wo_stop[i] = ''
+			for j in range(len(queries_split[i])):
+				s = ''.join(filter(str.isalnum, queries_split[i][j]))				
+				if not s in self.stop_words:
+					s = stemmer.stem(s)
+					if j == len(queries_split[i]) - 1:
+						queries_split_wo_stop[i] = queries_split_wo_stop[i] + s 
+					else:
+						queries_split_wo_stop[i] = queries_split_wo_stop[i] + s + ' '
+
+		# Store final processed queries with query objects
+		self.processed_queries = []
+		qid = 0
+		for q in queries_split_wo_stop:
+			new_q = Query.Query()
+			new_q.setTopicId(qid)
+			new_q.setQueryContent(q)
+			self.processed_queries.append(new_q)
+			qid = qid + 1
+
+	# Return extracted queries with class Query in a list.
+	def getQuries(self):
+		# already did the processing, just return processed queries 
+		return self.processed_queries
+
---
+++ index_engine/Search/QueryRetreivalModel.py
@@ -0,0 +1,83 @@
+import Classes.Query as Query
+import Classes.Document as Document
+import numpy as np
+
+class QueryRetrievalModel:
+
+    indexReader=[]
+
+    def __init__(self, ixReader):
+        self.indexReader = ixReader
+        self.mu = 2000                 # for smoothing
+
+        # Get all document lengths and store in list
+        self.doc_lengths = []
+        i = 0
+        while True:
+            try:
+                self.doc_lengths.append(self.indexReader.getDocLength(i))
+                i = i + 1
+            except:
+                break
+        self.total_doc_count = i
+        self.entire_collection_word_count = sum(self.doc_lengths)
+
+
+    # query:  The query to be searched for.
+    # topN: The maximum number of returned documents.
+    # The returned results (retrieved documents) should be ranked by the score (from the most relevant to the least).
+    # You will find our IndexingLucene.Myindexreader provides method: docLength().
+    # Returned documents should be a list of Document.
+    def retrieveQuery(self, query, topN):
+
+        # Get query terms by splitting query content
+        query_terms = query.queryContent.split()
+
+        # Dictionaries represent word counts c(qi in d) and p(qi | d)
+        word_counts_matrix = dict()         # c(qi in d)
+        prob_word_in_doc_matrix = dict()    # p(qi | d) 
+
+        # This contains P(Q|Md) per document (prod of all p(qi in d))
+        overall_query_probs = np.ones((self.total_doc_count,))
+
+        # Loop through each query term 
+        for q in query_terms:
+
+            # If word is not in collection, ignore
+            collection_count = self.indexReader.CollectionFreq(q)
+            if collection_count == 0:
+                continue
+
+            # Initialize matrix for term with numpy array of 0s (represent term counts and probs for each doc)
+            word_counts_matrix[q] = np.zeros((self.total_doc_count,))
+            prob_word_in_doc_matrix[q] = np.zeros((self.total_doc_count,))
+        
+            # Get postings list and counts # of words per document; index into word_counts_matrix with counts
+            post_list = self.indexReader.getPostingList(q)
+            for i in post_list.keys():
+                word_count_in_this_doc = post_list[i]
+                word_counts_matrix[q][int(i)] = word_count_in_this_doc
+
+            # Compute prob of seeing word across entire collection
+            prob_query_in_entire_collection = np.sum(word_counts_matrix[q]) / self.entire_collection_word_count
+            
+            # Compute P(Q|Md) = prod P(qi|Md); P(qi|Md) = (c(w;d) + mu*p(w|C)) / ((sum_w c(w;d))+mu) 
+            # Query likelihood with Dirichlet smoothing
+            for i in range(self.total_doc_count):
+                prob_word_in_doc_matrix[q][int(i)] = (word_counts_matrix[q][int(i)] + (self.mu*prob_query_in_entire_collection)) / (self.doc_lengths[i] + self.mu)
+                overall_query_probs[int(i)] = overall_query_probs[int(i)] * prob_word_in_doc_matrix[q][int(i)]
+
+        # Get idx of topN docs in terms of P(Q|Md)
+        idx = np.argpartition(overall_query_probs, -topN)[-topN:]   
+        idx = np.flip(idx[np.argsort(overall_query_probs[idx])])       # to make sure in correct order
+
+        # Organize results into list of document objects
+        docs = []
+        for i in range(len(idx)):
+            doc_i = Document.Document()
+            doc_i.setDocId(idx[i])
+            doc_i.setDocNo(self.indexReader.getDocNo(idx[i]))
+            doc_i.setScore(overall_query_probs[idx[i]])
+            docs.append(doc_i)
+
+        return docs
