b134212305acc2754a8f02f3cbce464a86fe749d
BenjaminTruckenbrod <79273097+BenjaminTruckenbrod@users.noreply.github.com>
2022-04-15 17:50:10 -0400

    Add files via upload
    This file give the functionality to query the index based on user input and select the retrieval model between Boolean and BM25.

---
+++ index_engine/query_index.py
@@ -0,0 +1,67 @@
+import IndexingWithWhoosh.MyIndexReader as MyIndexReader
+import SearchWithWhoosh.QueryRetreivalModel as QueryRetreivalModel
+import SearchWithWhoosh.QueryRetreivalModelBoolean as QueryRetreivalModelBoolean
+import SearchWithWhoosh.ExtractQuery as ExtractQuery
+import Classes.Query as Query
+from nltk.stem.porter import PorterStemmer
+import datetime
+
+# Removing nonalphanumeric chars may not be good if symbols are of value
+
+
+def clean_content(query, make_lowercase=True, stem=True, remove_nonalpha=True):
+    stemmer = PorterStemmer()
+    query_list = query.split()
+    clean_string = ''
+
+    for term in query_list:
+        a = term
+        if make_lowercase:
+            a = term.lower()
+        if stem:
+            a = stemmer.stem(a)
+        if remove_nonalpha:
+            a = ''.join(filter(str.isalnum, a))
+        clean_string += a + ' '
+
+    return clean_string
+
+
+def get_query():
+    query = input("Enter your query: ")
+    return query
+
+
+def set_query(query):
+    aQuery = Query.Query()
+    aQuery.setQueryContent(query)
+    aQuery.setTopicId('Topic 1')
+    return aQuery
+
+
+index = MyIndexReader.MyIndexReader("trectext")
+
+model = input("Boolean or BM25? ")
+q = get_query()
+clean = clean_content(q)
+query = set_query(clean)
+
+if (model == "Boolean"):
+    search = QueryRetreivalModelBoolean.QueryRetrievalModelBoolean(index)
+    results = search.retrieveQuery(query, 20)
+    rank = 1
+    for result in results:
+        print(query.getTopicId(), " Q0 ", result.getDocNo(),
+              ' ', rank, " ", " MYRUN",)
+        rank += 1
+        if (rank == 21):
+            break
+
+else:
+    search = QueryRetreivalModel.QueryRetrievalModel(index)
+    results = search.retrieveQuery(query, 20)
+    rank = 1
+    for result in results:
+        print(query.getTopicId(), " Q0 ", result.getDocNo(),
+              ' ', rank, " ", result.getScore(), " MYRUN",)
+        rank += 1
