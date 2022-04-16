da8b032cd9fc271b9b8adc1365ccd8ebd028c90a
BenjaminTruckenbrod <79273097+BenjaminTruckenbrod@users.noreply.github.com>
2022-04-16 11:45:13 -0400

    Update covid_index.py

--- index_engine/covid_index.py
+++ index_engine/covid_index.py
@@ -1,59 +1,72 @@
-import IndexingWithWhoosh.PreProcessedCorpusReader as PreprocessedCorpusReader
-import IndexingWithWhoosh.MyIndexWriter as MyIndexWriter
 import IndexingWithWhoosh.MyIndexReader as MyIndexReader
-import datetime
+import SearchWithWhoosh.QueryRetreivalModel as QueryRetreivalModel
+import SearchWithWhoosh.QueryRetreivalModelBoolean as QueryRetreivalModelBoolean
+import SearchWithWhoosh.ExtractQuery as ExtractQuery
+import Classes.Query as Query
 from nltk.stem.porter import PorterStemmer
+import datetime
 
-def WriteIndex(type):
-    count = 0
-    # Initiate pre-processed collection file reader.
-    corpus =PreprocessedCorpusReader.PreprocessedCorpusReader(type)
-    # Initiate the index writer.
-    indexWriter = MyIndexWriter.MyIndexWriter(type)
-    # Build index of corpus document by document.
-    while True:
-        doc = corpus.nextDocument()
-        if doc == None:
+# Removing nonalphanumeric chars may not be good if symbols are of value
+
+
+def clean_content(query, make_lowercase=True, stem=True, remove_nonalpha=True):
+    bool_ops = ['AND', 'OR', 'NOT', 'ANDNOT', 'ANDMAYBE']
+    stemmer = PorterStemmer()
+    query_list = query.split()
+    clean_string = ''
+
+    for term in query_list:
+        if (term not in bool_ops):
+            a = term
+            if make_lowercase:
+                a = term.lower()
+            if stem:
+                a = stemmer.stem(a)
+            if remove_nonalpha:
+                a = ''.join(filter(str.isalnum, a))
+            clean_string += a + ' '
+
+    return clean_string
+
+
+def get_query():
+    query = input("Enter your query: ")
+    return query
+
+
+def set_query(query):
+    aQuery = Query.Query()
+    aQuery.setQueryContent(query)
+    aQuery.setTopicId('Topic 1')
+    return aQuery
+
+
+index = MyIndexReader.MyIndexReader("trectext")
+
+model = input("Boolean or BM25? ")
+q = get_query()
+clean = clean_content(q)
+query = set_query(q)
+
+if (model == "Boolean"):
+    limit = int(input("How many documents would you like returned? "))
+    limit = 100
+    search = QueryRetreivalModelBoolean.QueryRetrievalModelBoolean(index)
+
+    results = search.retrieveQuery(query, 20)
+    rank = 1
+    for result in results:
+        print(query.getTopicId(), " Q0 ", result.getDocNo(),
+              ' ', rank, " ", " MYRUN",)
+        rank += 1
+        if (rank == limit+1):
             break
-        indexWriter.index(doc[0], doc[1])
-        count+=1
-        if count%1000==0:
-            print("finish ", count," docs")
-    print("totally finish ", count, " docs")
-    indexWriter.close()
-    return
 
-# IMPORTANT - MAKE THIS SAME AS DOC PARSER - TO DO
-def clean_content(term, make_lowercase=True, stem=True, remove_nonalpha=True):   # Removing nonalphanumeric chars may not be good if symbols are of value
-        stemmer = PorterStemmer()
-        clean_content = term
-        if make_lowercase:
-            clean_content = clean_content.lower()
-        if stem:
-            clean_content = stemmer.stem(term)
-        if remove_nonalpha: 
-            clean_content = ''.join(filter(str.isalnum, term))
-        return clean_content
-
-def ReadIndex(type, token):
-    token = clean_content('mask')
-    # Initiate the index file reader.
-    index = MyIndexReader.MyIndexReader(type)
-    # retrieve the token.
-    df = index.DocFreq(token)
-    ctf = index.CollectionFreq(token)
-    print(" >> the token \""+token+"\" appeared in "+ str(df) +" documents and "+ str(ctf) +" times in total")
-    if df>0:
-        posting = index.getPostingList(token)
-        for docId in posting:
-            docNo = index.getDocNo(docId)
-            print(docNo+"\t"+str(docId)+"\t"+str(posting[docId]))
-
-startTime = datetime.datetime.now()
-WriteIndex("trectext")
-endTime = datetime.datetime.now()
-print ("index web corpus running time: ", endTime - startTime)
-startTime = datetime.datetime.now()
-ReadIndex("trectext", "pandemic")
-endTime = datetime.datetime.now()
-print ("load index & retrieve the token running time: ", endTime - startTime)
+else:
+    search = QueryRetreivalModel.QueryRetrievalModel(index)
+    results = search.retrieveQuery(query, 20)
+    rank = 1
+    for result in results:
+        print(query.getTopicId(), " Q0 ", result.getDocNo(),
+              ' ', rank, " ", result.getScore(), " MYRUN",)
+        rank += 1
