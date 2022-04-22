9c97d3097c41ff3b1ffa78f89dc2e7311816799a
krbuettner <78238895+krbuettner@users.noreply.github.com>
2022-04-22 15:12:15 -0400

    Update and rename parse_data.py to sample_cord19.py

--- document_parsing/sample_cord19.py
+++ document_parsing/sample_cord19.py
@@ -1,14 +1,13 @@
-
-# This is tailored to my repo, but the general idea is there
-
 import os
 import csv
 import random
 import shutil
 
-# Import configs
+# Replace these with your paths of interest, assuming cord19 is downloaded into cord19 directory
 path_to_data = '/archive2/kyle/cord19/document_parses/pdf_json/'
 new_path = '/archive2/kyle/cord19/document_parses_10000/'
+
+# Set up variables
 list_of_file_names = []
 num_samples = 10000
 
@@ -23,12 +22,3 @@ random_sample = random.sample(list_of_file_names, num_samples)
 for f in random_sample:
     print(f)
     shutil.copyfile(path_to_data + f, new_path + f)
-
-# Some functionality for reading csv files
-#for csv_file_name in os.listdir(path_to_data):
-#    print(csv_file_name)
-#    with open(path_to_data + csv_file_name, newline='') as csvfile:
-#        reader = csv.reader(csvfile, delimiter=' ',quotechar='|')
-#        for row in reader:
-#            print(row)
-#        exit()
