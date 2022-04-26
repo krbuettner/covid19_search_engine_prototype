e3d4b6342d1ab4323cf95c336b94d4c7438e81bf
krbuettner <78238895+krbuettner@users.noreply.github.com>
2022-04-26 12:04:28 -0400

    clean up paths

--- index_engine/Classes/Path.py
+++ index_engine/Classes/Path.py
@@ -1,17 +1,3 @@
-
-# uncompleted address of preprocessed corpus.
-#ResultHM1="data//result."
-
-# address of generated Text index file.
 IndexTextDir="data//indextext//"
-
-# address of generated Web index file.
-IndexWebDir="data//indexweb//"
-
 ResultHM1="data//docs_w_dates.txt"
-
-# Added topic directory
-TopicDir = "data//topics.txt"
-
-# Added stopword directory
 StopwordDir = "data//stopword.txt"
