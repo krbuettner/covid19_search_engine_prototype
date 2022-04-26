74f2b0658adc7153333a93cc659d842b9799697d
krbuettner <78238895+krbuettner@users.noreply.github.com>
2022-04-26 12:01:41 -0400

    cleanup

--- index_engine/Search/QueryRetreivalModelBoolean.py
+++ index_engine/Search/QueryRetreivalModelBoolean.py
@@ -20,9 +20,6 @@ class QueryRetrievalModelBoolean:
 
     # query:  The query to be searched for.
     # topN: The maximum number of returned documents.
-    # The returned results (retrieved documents) should be ranked by the score (from the most relevant to the least).
-    # You will find our IndexingLucene.Myindexreader provides method: docLength().
-    # Returned documents should be a list of Document.
 
     def retrieveQuery(self, query, min_time=None):
         corrector = self.searcher.corrector("doc_content")
